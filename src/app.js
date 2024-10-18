d4d3b0ffb6c5acb8bcc063db7316d3b0734824a0
mariammartirosyan16 <mariammartirosyan1988@mail.ru>
2024-10-18 22:41:56 +0400

    playing witt routs

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userID/:name", (req, res) => {
+    console.log(req.params)
   res.send({ fistName: "Mariam", lastName: "Martirsoyan" });
 });
-app.post("/user", (req, res) => {
-  res.send("Data saved to DB");
-});
-app.delete("/user", (req, res) => {
-    res.send("Data deleted.");
-  });
+// app.post("/user", (req, res) => {
+//   res.send("Data saved to DB");
+// });
+// app.delete("/user", (req, res) => {
+//     res.send("Data deleted.");
+//   });
 //handle all routes
-app.use((req, res) => {
-  res.send("Hello!");
-});
+// app.use((req, res) => {
+//   res.send("Hello!");
+// });
 
 app.listen(3000, () => {
   console.log("Server is  listen to port 3000");
